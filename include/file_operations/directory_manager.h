631849457f759b7e59e0544decf07d917fd70118
andin0 <2576470312@qq.com>
2025-06-04 23:07:49 +0800

    新增两个const

--- include/file_operations/directory_manager.h
+++ include/file_operations/directory_manager.h
@@ -16,8 +16,8 @@ public:
     DirectoryManager(DataBlockManager *dbManager, InodeManager *inodeManager, SuperBlockManager *sbManager);
     bool addEntry(Inode &parentDirInode, const std::string &name, int entryInodeId, FileType type); // FileType 在 common_defs.h
     bool removeEntry(Inode &parentDirInode, const std::string &name);
-    int findEntry(Inode &dirInode, const std::string &name);
-    std::vector<DirectoryEntry> listEntries(Inode &dirInode);                                                           // DirectoryEntry 在 data_structures.h
+    int findEntry(Inode &dirInode, const std::string &name) const;
+    std::vector<DirectoryEntry> listEntries(Inode &dirInode) const;                                                           // DirectoryEntry 在 data_structures.h
     int resolvePathToInode(const std::string &path, int currentDirInodeId, int rootDirInodeId, const User *currentUser, // User 在 data_structures.h
                            int *parentInodeId = nullptr, std::string *lastName = nullptr, bool followLastLink = true);
     int createDirectoryInode(short ownerUid, short permissions);
